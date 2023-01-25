07401eb80c508084dd219ab4e04e1764aea3a3d4
youbuntu <mkipeo@gmail.com>
2023-01-25 18:22:25 +0900

    route에 transition 설정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar />
-    <router-view />
+    <transition name="page">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -19,5 +21,15 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* Router Transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter,
+.page-leave-to {
+  opacity: 0;
 }
 </style>
