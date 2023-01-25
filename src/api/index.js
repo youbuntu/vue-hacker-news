8eb22a258aa7522ff44ff7acb1afff19cc3efe3a
youbuntu <mkipeo@gmail.com>
2023-01-25 18:21:30 +0900

    api 추가

--- src/api/index.js
+++ src/api/index.js
@@ -18,4 +18,18 @@ function fetchAskList() {
   return axios.get(`${config.baseUrl}/ask/1.json`);
 }
 
-export { fetchNewsList, fetchJobsList, fetchAskList };
+function fetchUserInfo(userName) {
+  return axios.get(`${config.baseUrl}/user/${userName}.json`);
+}
+
+function fetchCommentItem(id) {
+  return axios.get(`${config.baseUrl}/item/${id}.json`);
+}
+
+export {
+  fetchNewsList,
+  fetchJobsList,
+  fetchAskList,
+  fetchUserInfo,
+  fetchCommentItem,
+};
