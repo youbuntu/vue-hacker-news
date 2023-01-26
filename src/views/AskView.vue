58fec30c4573da8f40f4d4c1f4b77e8a24eac5d3
youbuntu <mkipeo@gmail.com>
2023-01-26 14:54:00 +0900

    ListItem 공통 컴포넌트화

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,81 +1,18 @@
 <template>
   <div>
-    <ul class="news-list">
-      <li class="post" v-for="item in fetchedAsk" v-bind:key="item.id">
-        <!-- 포인트 영역 -->
-        <div class="points">
-          {{ item.points }}
-        </div>
-        <!-- 기타 정보 영역 -->
-        <div>
-          <p class="news-title">
-            <router-link v-bind:to="`/item/${item.id}`">
-              {{ item.title }}
-            </router-link>
-          </p>
-          <small class="link-text">
-            by
-            <router-link class="link-text" v-bind:to="`/user/${item.user}`">
-              {{ item.user }}
-            </router-link>
-          </small>
-        </div>
-      </li>
-    </ul>
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import ListItem from "../components/ListItem.vue";
 
 export default {
-  computed: {
-    ...mapGetters(["fetchedAsk"]),
-
-    // #3
-    // ...mapGetters({
-    //   ask: "fetchedAsk",
-    // }),
-
-    // #2
-    // ...mapState({
-    //   ask: (state) => state.ask,
-    // }),
-
-    // #1
-    // ask() {
-    //   return this.$store.state.ask;
-    // },
-  },
-  created() {
-    this.$store.dispatch("FETCH_ASK");
+  components: {
+    ListItem,
   },
 };
 </script>
 
 <style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-}
 </style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,65 +1,18 @@
 <template>
   <div>
-    <ul class="news-list">
-      <li
-        class="post"
-        v-for="job in this.$store.state.jobs"
-        v-bind:key="job.id"
-      >
-        <!-- 포인트 영역 -->
-        <div class="points">
-          {{ job.points ?? 0 }}
-        </div>
-        <!-- 기타 정보 영역 -->
-        <div>
-          <p class="news-title">
-            <a v-bind:href="job.url">
-              {{ job.title }}
-            </a>
-          </p>
-          <small class="link-text">
-            by
-            <a v-bind:href="job.url">
-              {{ job.domain }}
-            </a>
-          </small>
-        </div>
-      </li>
-    </ul>
+    <list-item></list-item>
   </div>
 </template>
 
 <script>
+import ListItem from "../components/ListItem.vue";
+
 export default {
-  created() {
-    this.$store.dispatch("FETCH_JOBS");
+  components: {
+    ListItem,
   },
 };
 </script>
 
 <style scoped>
-.news-list {
-  margin: 0;
-  padding: 0;
-}
-.post {
-  list-style: none;
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid #eee;
-}
-.points {
-  width: 80px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #42b883;
-}
-.news-title {
-  margin: 0;
-}
-.link-text {
-  color: #828282;
-}
 </style>
